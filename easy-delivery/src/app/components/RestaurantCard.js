99221206c972238e2147d443cf87824d2ebae211
Armaity Khurshed Katki <131193721+armaitykk@users.noreply.github.com>
2024-03-30 16:43:38 -0400

    starting page

---
+++ easy-delivery/src/app/components/RestaurantCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './RestaurantCard.css';
+
+const RestaurantCard = ({ imageSrc, restaurantName, rating, price }) => {
+  return (
+    <div className={styles.card}>
+      <img src={imageSrc} alt="Restaurant" className={styles.image} />
+      <div className={styles.details}>
+        <h2 className={styles.restaurantName}>{restaurantName}</h2>
+        <div className={styles.rating}>{rating} stars</div>
+        <div className={styles.price}>Price: {price}</div>
+      </div>
+    </div>
+  );
+}
+
+export default RestaurantCard;
