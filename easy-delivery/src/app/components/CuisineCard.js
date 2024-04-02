dfe448655133540fa0d390c9ff887c9f74fbdd57
Esha Patel <esha@Eshas-MacBook-Pro.local>
2024-04-02 09:35:25 -0400

    routeronpage

--- easy-delivery/src/app/components/CuisineCard.js
+++ easy-delivery/src/app/components/CuisineCard.js
@@ -1,3 +1,4 @@
+'use client'
 import React from "react";
 import './CuisineCard.css';
 import { MaterialIcons } from '@expo/vector-icons';
--- easy-delivery/src/app/page.js
+++ easy-delivery/src/app/page.js
@@ -1,17 +1,23 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import RestaurantCard from "./components/RestaurantCard";
-
+'use client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LogIn from './components/LogIn';
+import styles from './page.module.css';
+import RestaurantCard from './components/RestaurantCard';
 
 function Home() {
   return (
     <div className={styles.container}>
-      <RestaurantCard
+      {/* <RestaurantCard
         imageSrc="url_to_your_image.jpg"
         restaurantName="Your Restaurant Name"
         rating={4.5}
         price="$$"
-      />
+      /> */}
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<LogIn />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
