4115170595ba0f5317c276c5387c49c91179cbbb
Armaity Khurshed Katki <131193721+armaitykk@users.noreply.github.com>
2024-04-07 10:33:16 -0400

    styling form

--- easy-delivery/src/app/log-in/page.js
+++ easy-delivery/src/app/log-in/page.js
@@ -7,12 +7,6 @@ import Navigation from '../components/Navigation';
 function Home() {
   return (
     <div className={styles.container}>
-      {/* <RestaurantCard
-        imageSrc="url_to_your_image.jpg"
-        restaurantName="Your Restaurant Name"
-        rating={4.5}
-        price="$$"
-      /> */}
       <Navigation />
       <LogIn />
 
