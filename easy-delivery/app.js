ef4fd54eb77d00bcf811c90289afced699d95a47
Youran Roh <109929835+youranroh@users.noreply.github.com>
2024-04-17 17:45:00 -0400

    DB

---
+++ easy-delivery/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('Hello world!'));
+const port = process.env.PORT || 8082;
+//const mongoose = require('mongoose');
+//const cors =require('cors');
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
+//app.use (cors({origin:true, credentials: true}));
+//app.get('/', (req,res) => res.send('Hello World!'));
+//app.use(express.json({extended:false})); //required line
+
+//const items = require('./routes/api/items');
+//app.use('/api/items', items);
+
+//const conn_str = 'mongodb+srv://youran:1111@cluster0.4w3xybo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+/*
+mongoose.set('strictQuery',false);
+mongoose.connect(conn_str).then(()=>{
+    app.listen(port, () => console.log(`Server running on port ${port}`));
+    console.log('MongoDB Connection Succeeded...')
+})
+.catch(err => {
+    console.log('Error in DB connection');
+});
+*/
